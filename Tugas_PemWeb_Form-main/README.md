006ca374dddc7e4597d5b708f62b2f871ac45771
nayla zalfa <naylazalfa03@gmail.com>
2026-04-21 14:04:08 +0700

    initial commit

---
+++ Tugas_PemWeb_Form-main/README.md
@@ -0,0 +1,3 @@
+![gambar form](./src/Screenshot/gambar.jpeg)
+![gambar form](./src/Screenshot/imagehasil.png)
+![gambar form](./src/Screenshot/image1.png)
---
+++ Tugas_PemWeb_Form-main/src/App.tsx
@@ -0,0 +1,11 @@
+import RegisterForm from "./components/organism/RegisterForm";
+
+function App() {
+  return (
+    <div className="p-10">
+      <RegisterForm />
+    </div>
+  );
+}
+
+export default App;
